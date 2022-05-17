7d992a1da6d9ee094b88b67fd471d67db839cafc
CallistaWidya <105658865+CallistaWidya@users.noreply.github.com>
2022-05-17 22:48:09 +0700

    Add files via upload

---
+++ pertemuan-2/Latihan 1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Biodata Idola")
+
+nama="Taylor Swift"
+TTL="Pennsylvania, 13 Desember 1989"
+umur=33
+tinggi="180 cm"
+profesi="Penyanyi, Penulis lagu, Aktris"
+
+print("Nama : ",nama)
+print("TTL : ",TTL)
+print("Umur : ",umur,"Tahun")
+print("Tinggi Badan : ",tinggi)
+print("Profesi : ",profesi)
+
+
+# In[ ]:
+
+
+
+
